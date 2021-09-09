af6b69a6ec6131dbf7b5e93663b36ac0f96a9c0f
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-09-09 13:35:56 +0400

    style: refactor flex to Grid

--- src/components/TicTakToe/TicTakToe.jsx
+++ src/components/TicTakToe/TicTakToe.jsx
@@ -4,21 +4,17 @@ import styles from "./TicTakToe.module.css";
 export function TicTakToe() {
   return (
     <div className={styles.board}>
-      <div className={styles.row}>
-        <Square symbol="O" />
-        <Square symbol="X" />
-        <Square symbol="O" />
-      </div>
-      <div className={styles.row}>
-        <Square symbol="X" />
-        <Square symbol={null} />
-        <Square symbol="X" />
-      </div>
-      <div className={styles.row}>
-        <Square symbol="X" />
-        <Square symbol="X" />
-        <Square symbol="X" />
-      </div>
+      <Square symbol="O" />
+      <Square symbol="X" />
+      <Square symbol="O" />
+
+      <Square symbol="X" />
+      <Square symbol={null} />
+      <Square symbol="X" />
+
+      <Square symbol="X" />
+      <Square symbol="X" />
+      <Square symbol="X" />
     </div>
   );
 }
