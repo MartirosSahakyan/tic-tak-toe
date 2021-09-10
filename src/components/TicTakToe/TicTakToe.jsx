0e003814bc2c33402f5608dce720d0331797ce8e
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-09-10 18:26:39 +0400

    refactor(TicTacToe): change variable player to currPlayer

--- src/components/TicTakToe/TicTakToe.jsx
+++ src/components/TicTakToe/TicTakToe.jsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./TicTakToe.module.css";
-import { Square } from "../Square/Square";
 import {
   changeMatrixElement,
   checkDraw,
   emptyMatrix,
   findWinner,
 } from "../../helpers/helper";
-import { useEffect } from "react";
 import NotificationIsDraw from "../NotificationIsDraw/NotificationIsDraw";
+import Square  from "../Square/Square";
 import CurrentPlayer from "../CurrentPlayer/CurrentPlayer";
 import NotificationWin from "../NotificationWin/NotificationWin";
+import ChoosePlayer from "../ChoosePlayer/ChoosePlayer";
 
 export function TicTakToe() {
   const [board, setBoard] = useState([
@@ -18,47 +18,48 @@ export function TicTakToe() {
     [null, null, null],
     [null, null, null],
   ]);
-  const [player, setPlayer] = useState("X");
+  // const [startWith, setStartWith] = useState("X")
+  const [currPlayer, setCurrPlayer] = useState(startWith);
   const [isDraw, setIsDraw] = useState(false);
   const [winner, setWinner] = useState("");
 
-  const handleSquareClick = (i, player) => {
+  const handleSquareClick = (i, currPlayer) => {
     switch (i) {
       case 0:
-        setBoard(changeMatrixElement(board, 0, 0, player));
-        if (!board[0][0]) setPlayer((player = player === "X" ? "O" : "X"));
+        setBoard(changeMatrixElement(board, 0, 0, currPlayer));
+        if (!board[0][0]) setCurrPlayer((currPlayer = currPlayer === "X" ? "O" : "X"));
         break;
       case 1:
-        setBoard(changeMatrixElement(board, 0, 1, player));
-        if (!board[0][1]) setPlayer((player = player === "X" ? "O" : "X"));
+        setBoard(changeMatrixElement(board, 0, 1, currPlayer));
+        if (!board[0][1]) setCurrPlayer((currPlayer = currPlayer === "X" ? "O" : "X"));
         break;
       case 2:
-        setBoard(changeMatrixElement(board, 0, 2, player));
-        if (!board[0][2]) setPlayer((player = player === "X" ? "O" : "X"));
+        setBoard(changeMatrixElement(board, 0, 2, currPlayer));
+        if (!board[0][2]) setCurrPlayer((currPlayer = currPlayer === "X" ? "O" : "X"));
         break;
       case 3:
-        setBoard(changeMatrixElement(board, 1, 0, player));
-        if (!board[1][0]) setPlayer((player = player === "X" ? "O" : "X"));
+        setBoard(changeMatrixElement(board, 1, 0, currPlayer));
+        if (!board[1][0]) setCurrPlayer((currPlayer = currPlayer === "X" ? "O" : "X"));
         break;
       case 4:
-        setBoard(changeMatrixElement(board, 1, 1, player));
-        if (!board[1][1]) setPlayer((player = player === "X" ? "O" : "X"));
+        setBoard(changeMatrixElement(board, 1, 1, currPlayer));
+        if (!board[1][1]) setCurrPlayer((currPlayer = currPlayer === "X" ? "O" : "X"));
         break;
       case 5:
-        setBoard(changeMatrixElement(board, 1, 2, player));
-        if (!board[1][2]) setPlayer((player = player === "X" ? "O" : "X"));
+        setBoard(changeMatrixElement(board, 1, 2, currPlayer));
+        if (!board[1][2]) setCurrPlayer((currPlayer = currPlayer === "X" ? "O" : "X"));
         break;
       case 6:
-        setBoard(changeMatrixElement(board, 2, 0, player));
-        if (!board[2][0]) setPlayer((player = player === "X" ? "O" : "X"));
+        setBoard(changeMatrixElement(board, 2, 0, currPlayer));
+        if (!board[2][0]) setCurrPlayer((currPlayer = currPlayer === "X" ? "O" : "X"));
         break;
       case 7:
-        setBoard(changeMatrixElement(board, 2, 1, player));
-        if (!board[2][1]) setPlayer((player = player === "X" ? "O" : "X"));
+        setBoard(changeMatrixElement(board, 2, 1, currPlayer));
+        if (!board[2][1]) setCurrPlayer((currPlayer = currPlayer === "X" ? "O" : "X"));
         break;
       case 8:
-        setBoard(changeMatrixElement(board, 2, 2, player));
-        if (!board[2][2]) setPlayer((player = player === "X" ? "O" : "X"));
+        setBoard(changeMatrixElement(board, 2, 2, currPlayer));
+        if (!board[2][2]) setCurrPlayer((currPlayer = currPlayer === "X" ? "O" : "X"));
         break;
       default:
         break;
@@ -71,21 +72,25 @@ export function TicTakToe() {
     setWinner("");
   };
 
+  // const handleChangePlayer = (event) => {
+  //   setStartWith(event.target.value);
+  // };
+
   useEffect(() => {
     if (findWinner(board)) {
       setWinner(findWinner(board));
-
     }else if (checkDraw(board)) {
       setIsDraw(!isDraw)
-    }
-    
+    }    
   }, [board]);
 
   return (
     <>
       {isDraw && <NotificationIsDraw onClick={handleStartAgain} />}
+      
+      <ChoosePlayer value={startWith} handleChange={handleChangePlayer} />
 
-      <CurrentPlayer player={player} />
+      <CurrentPlayer player={currPlayer} />
      
       {winner && <NotificationWin winner={winner} onClick={handleStartAgain} />}
      
@@ -95,7 +100,7 @@ export function TicTakToe() {
             <Square
               key={index}
               symbol={square}
-              player={player}
+              player={currPlayer}
               i={index}
               onClick={handleSquareClick}
             />
