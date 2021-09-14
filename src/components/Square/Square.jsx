4b1d6f3111cc9f730f2de76f8eee2dac6ff7b2e5
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-09-14 14:42:45 +0400

    style(Square.jsx): style game board

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,8 +1,16 @@
 import styles from "./Square.module.css";
+import cn from "classnames";
 
 export default function Square({ symbol, i, onClick, player }) {
   return (
-    <div className={styles.square} onClick={() => onClick(i, player)}>
+    <div
+      className={cn(
+        [styles.square],
+        { [styles.squareX]: player === "X" && !symbol },
+        { [styles.squareO]: player === "O" && !symbol }
+      )}
+      onClick={() => onClick(i, player)}
+    >
       <span className={styles.symbol}>{symbol}</span>
     </div>
   );
